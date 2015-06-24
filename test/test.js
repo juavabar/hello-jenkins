2aed53adfd7857385168dc583f9bce9de6e5a774
jvalero <jvalero@iti.es>
2015-06-24 18:17:32 +0200

    cambios de test

--- test/test.js
+++ test/test.js
@@ -2,7 +2,13 @@ var request = require('supertest');
 var app = require('../app.js');
  
 describe('GET /', function() {
+
   it('respond with hello world', function(done) {
     request(app).get('/').expect('hola radiola', done);
   });
+
+  it('forzar un fallo', function(done) {
+    done(new Error('JJJ Error forzado'));
+  })
+
 });
