e130cec01807b6547513a390c3aff0f31aeb36d9
jvalero <jvalero@iti.es>
2015-06-24 18:52:43 +0200

    Pruebas

--- test/test.js
+++ test/test.js
@@ -7,6 +7,10 @@ describe('GET /', function() {
     request(app).get('/').expect('hola-radiola', done);
   });
 
+  it('respond with hola-radiola', function(done) {
+    request(app).get('/').expect('hola-radiola-falla', done);
+  });
+
   // it('forzar un fallo', function(done) {
   //   done(new Error('JJJ Error forzado'));
   // })
