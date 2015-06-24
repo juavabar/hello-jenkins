aa3523014300e307864878c7e9ce7409dc6fffd7
jvalero <jvalero@iti.es>
2015-06-24 18:50:09 +0200

    Cambios menores

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ var app = require('../app.js');
  
 describe('GET /', function() {
 
-  it('respond with hello world', function(done) {
-    request(app).get('/').expect('hola radiola', done);
+  it('respond with hola-radiola', function(done) {
+    request(app).get('/').expect('hola-radiola', done);
   });
 
   // it('forzar un fallo', function(done) {
