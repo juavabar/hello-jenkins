cecaf8ce09cc382a95816c0abdf1437e97a84bfc
jvalero <jvalero@iti.es>
2015-06-25 09:19:37 +0200

    prueba

--- test/test.js
+++ test/test.js
@@ -4,12 +4,12 @@ var app = require('../app.js');
 describe('GET /', function() {
 
   it('respond with hola-radiola', function(done) {
-    request(app).get('/').expect(
-      'hola-radiola', 
-      setTimeout(done(), 5000)
-    );
+    request(app).get('/').expect('hola-radiola', done);
   });
 
+  it('forzar una pausa', function(done) {
+    setTimeout(done(), 4000);
+  });
 
   // it('forzar un fallo', function(done) {
   //   done(new Error('JJJ Error forzado'));
